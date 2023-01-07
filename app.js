912e0d6a9f8e62a304f3d1b53499e6c7fd56a3f0
Tommy Evertsen <94345410+TommyEvertsen@users.noreply.github.com>
2023-01-07 16:39:20 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function contact(){
-   
-    var contactDetails = []
+function contact() {
+  var contactDetails = [];
 
-       /*  Check what is written and store it in array */
-     for (var i = 0; i < document.forms[0].length; i++){
-        contactDetails.push  (document.forms[0].elements[i].value) 
-       contactDetails.push ('%0D%0A')
-} 
-   /*  Remove commas from array */
- var contactDetailsWithoutComma = contactDetails.join(''); 
- /* Open default email and pre populate the mail */
- window.open("mailto:tinyproductions.no@gmail.co.com?subject=Contact&body=" + contactDetailsWithoutComma); 
+  /*  Check what is written and store it in array */
+  for (var i = 0; i < document.forms[0].length; i++) {
+    contactDetails.push(document.forms[0].elements[i].value);
+    contactDetails.push("%0D%0A");
+  }
+  /*  Remove commas from array */
+  var contactDetailsWithoutComma = contactDetails.join("");
+  /* Open default email and pre populate the mail */
+  window.open(
+    "mailto:tinyproductions.no@gmail.co.com?subject=Contact&body=" +
+      contactDetailsWithoutComma
+  );
 }
